ebbf96cff5d1b42da583a3dc317feea2ece0112c
Ondra9 <155544125+Ondra9@users.noreply.github.com>
2024-03-08 22:28:50 +0100

    Update player_script.cs
    player_script2

--- player_script.cs
+++ player_script.cs
@@ -7,7 +7,12 @@ public class player_script : MonoBehaviour
 {
     public float speed;
     public float jump_speed;
-    public GameObject groundCheck;
+
+    public Collider2D pojistka2;
+
+    public Transform groundCheck;
+    public LayerMask groundLayer;
+    public float groundCheckRadius = 0.1f;
 
     private Rigidbody2D rb;
 
@@ -22,11 +27,12 @@ public class player_script : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
 
         //Debug.Log(moveHorizontal);
+        //Debug.Log(IsGrounded());
 
         Vector3 movement = Vector3.right * speed * moveHorizontal * Time.deltaTime;
         transform.Translate(movement);
 
-        if (Input.GetKeyDown(KeyCode.Space))
+        if (Input.GetKeyDown(KeyCode.Space) && IsGrounded())
         {
             Vector3 jump = Vector3.up * jump_speed;
             GetComponent<Rigidbody2D>().AddForce(jump, ForceMode2D.Impulse);
@@ -39,13 +45,18 @@ public class player_script : MonoBehaviour
         // Zde mùžete provést akce, které chcete provést pøi vstupu do triggeru
         Debug.Log("Vstoupil jsi do triggeru objektu: " + other.gameObject.name);
     }
-    /*
+    
     bool IsGrounded()
     {
+        Vector2 groundCheckPosition = new Vector2(groundCheck.position.x, groundCheck.position.y);
         // Zjistìní, zda se objekt dotýká zemì
-        //return Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+        return Physics2D.OverlapCircle(groundCheckPosition, groundCheckRadius, groundLayer);
     }
-    */
 
+    void OnTriggerEnter2D(Collider2D pojistka2)
+    {
+        // Zavolá se, když tento trigger vstoupí do jiného triggeru nebo kolize
+        Debug.Log("Nìco vstoupilo do triggeru.");
+    }
 }
 
