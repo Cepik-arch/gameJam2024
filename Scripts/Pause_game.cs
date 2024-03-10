c0ef264f845e0ba043ff72b603af81cf03ceac91
Ondra9 <155544125+Ondra9@users.noreply.github.com>
2024-03-10 12:23:49 +0100

    pausegame

--- Scripts/Pause_game.cs
+++ Scripts/Pause_game.cs
@@ -1,11 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Pause_game : MonoBehaviour
 {
     public Transform Camera_TR;
-
+    /*
     public GameObject paused_GO;
     public GameObject MusicOn;
     public GameObject MusicOff;
@@ -16,9 +17,22 @@ public class Pause_game : MonoBehaviour
     public GameObject MusicOn_N;
     public GameObject MusicOff_N;
     public GameObject SoundOn_N;
-    public GameObject SoundOff_N;
+    public GameObject SoundOff_N;*/
+
+    public GameObject paused_GO_D;
+    public GameObject paused_GO_N;
+
+    public GameObject Play_D;
+    public GameObject Exit_D;
+    public GameObject MusicOn_D;
+    public GameObject MusicOff_D;
+    public GameObject SoundOn_D;
+    public GameObject SoundOff_D;
+
 
     public bool paused = false;
+    public bool music = true;
+    public bool sound = true;
 
     //audio
     AudioSource[] audioSources = FindObjectsOfType<AudioSource>();
@@ -27,16 +41,119 @@ public class Pause_game : MonoBehaviour
 
     void Start()
     {
-        paused_GO.SetActive(false);
+        /*
+        paused_GO_D.SetActive(false);
         paused_GO_N.SetActive(false);
-        SoundOff.SetActive(false);
-        MusicOff.SetActive(false);
+
+        SoundOff_D.SetActive(false);
+        MusicOff:D.SetActive(false);
+
+        SoundOff_N.SetActive(false);
+        MusicOn_N.SetActive(false);*/
+        if (sound == true)
+        {
+            SoundOn_D.SetActive(true);
+            SoundOff_D.SetActive(false);
+        }
+        else
+        {
+            SoundOn_D.SetActive(false); 
+            SoundOff_D.SetActive(true);
+        }
+
+        if (music == true)
+        {
+            MusicOn_D.SetActive(true);
+            MusicOff_D.SetActive(false);
+        }
+        else
+        {
+            MusicOn_D.SetActive(false);
+            MusicOff_D.SetActive(true);
+        }
     }
+
+    ////////////  buttons pro destroyed world
+    public void Button_Play_D(GameObject Play_D)
+    {
+        Debug.Log("Tlaèítko bylo zmáèknuto! play_d");
+        if (Variables.realm == 0)
+        {
+            Debug.Log("Hra byla odpauznuta");
+            paused_GO_D.SetActive(false);
+            paused = false;
+            Time.timeScale = 1;
+        }
+        else if (Variables.realm == 1)
+        {
+            Debug.Log("Hra byla odpauznuta");
+            paused_GO_N.SetActive(false);
+            paused = false;
+            Time.timeScale = 1;
+        }
+    }
+    public void Button_Exit_D(GameObject Exit_D)
+    {
+        Debug.Log("Hra byla ukonèena.");
+        Application.Quit();
+    }
+    public void Button_MusicOn_D(GameObject MusicOn_D)
+    {
+        Debug.Log("hudbaon");
+        music = false;
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.CompareTag(MusicTag))
+            {
+                audioSource.enabled = false; // Vypneme Audio Source s odpovídajícím tagem
+            }
+        }
+    }
+
+    public void Button_MusicOff_D(GameObject MusicOff_D)
+    {
+        Debug.Log("hudbaoff");
+        music = true;
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.CompareTag(MusicTag))
+            {
+                audioSource.enabled = true; // Zapneme Audio Source s odpovídajícím tagem
+            }
+        }
+    }
+
+    public void Button_SoundOn_D(GameObject SoundOn_D)
+    {
+
+        Debug.Log("zvukOn");
+        sound = false;
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.CompareTag(SoundTag))
+            {
+                audioSource.enabled = false; // Vypneme Audio Source s odpovídajícím tagem
+            }
+        }
+    }
+    public void Button_SoundOff_D(GameObject SoundOff_D)
+    {
+        Debug.Log("zvukOff");
+        sound = true;
+        foreach (AudioSource audioSource in audioSources)
+        {
+            if (audioSource.CompareTag(SoundTag))
+            {
+                audioSource.enabled = false; // Vypneme Audio Source s odpovídajícím tagem
+            }
+        }
+    }
+
+
 
     void Update()
     {
-        //transform.position = Camera_TR.transform.position;
-
+        
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             if (Variables.realm == 0)
@@ -44,21 +161,46 @@ public class Pause_game : MonoBehaviour
                 Debug.Log("Paused");
                 Time.timeScale = 0;
                 paused = true;
-                paused_GO.SetActive(true);
-            }
+                paused_GO_D.SetActive(true);
+            }/*
             else if (Variables.realm == 1)
             {
                 Debug.Log("Paused");
                 Time.timeScale = 0;
                 paused = true;
                 paused_GO_N.SetActive(true);
-            }
+            }*/
         }
-        // Získání pozice kurzoru ve 2D prostoru
-        Vector2 cursorPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        // Vytvoøení paprsku od kamery do pozice kurzoru
-        RaycastHit2D hit = Physics2D.Raycast(cursorPosition, Vector2.zero);
 
+        // kontrola jestli je zapnutý zvuk a ikonek audia ve høe
+
+        if (sound == true && Variables.realm == 0)
+        {
+            Debug.Log(sound);
+            SoundOn_D.SetActive(true);
+            SoundOff_D.SetActive(false);
+        }
+        else if (sound == false && Variables.realm == 0)
+        {
+            Debug.Log(sound);
+            SoundOff_D.SetActive(true);
+            SoundOn_D.SetActive(false);
+        }
+
+        if (music == true && Variables.realm == 0)
+        {
+            Debug.Log(music);
+            MusicOn_D.SetActive(true);
+            MusicOff_D.SetActive(false);
+        }
+        else if (music == false && Variables.realm == 0)
+        {
+            Debug.Log(sound);
+            MusicOff_D.SetActive(true);
+            MusicOn_D.SetActive(false);
+        }
+
+        /*
         ///////////////// pause menu pro znièený svìt
 
         // pokraèování ve høe
@@ -219,10 +361,10 @@ public class Pause_game : MonoBehaviour
                     audioSource.enabled = true; // Zapneme Audio Source s odpovídajícím tagem
                 }
             }
-        }
+        }*/
 
-    }     
+
+    }
 }
 
 
-    
